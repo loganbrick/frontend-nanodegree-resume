0cb67586875a3df529b7dd4394a6f3803c3af517
Logan Brick <Logan Brick>
2016-10-09 16:32:52 -0500

    A commit before the resume builder was fleshed out to match (or closely match) that which is given an example in the Udacity course.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,43 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var awesomeThoughts;
+var funThoughts;
+var formattedName = HTMLheaderName.replace("%data%", "Logan Brick");
+var formattedRole = HTMLheaderRole.replace("%data%", "Electrical Engineer");
+
+awesomeThoughts="I'm Logan, and I am AWESOME MAN REALLY NEAT 100% FUNTIME JAM SESH."
+funThoughts = awesomeThoughts.replace("AWESOME", "FUN")
+
+//console.log(awesomeThoughts)
+//$("#main").append(funThoughts)
+
+var bio = {
+  "name" : "Logan Brick",
+  "role" : "Electrical Engineer",
+  "contacts" : {
+    "mobile" : "608-425-8243",
+    "email" : "logan.brick@gmail.com",
+    "github" : "https://github.com/loganbrick",
+    "twitter" : "@LoganBrick",
+    "location" : "Madison, WI"
+  },
+  "welcomeMessage" : "Welcome, friend. You're neat. Enjoy it here, dude.",
+  "skills" : [
+    "Math", "Complete Sobriety", "High-fiving"
+  ],
+  "bioPic" : "images/fry.jpg"
+}
+
+var work = {};
+work.position = "Friendly Chum";
+work.employer = "My friends";
+work.years = "24";
+
+var education = {};
+education["name"] = "University of Wisconsin, Madison";
+education["years"] = "2010-2014";
+education ["city"] = "Madison, Wisconsin";
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
+
+$("#header").prepend(formattedRole)
+$("#header").prepend(formattedName)
